4cb8d72bb367e7b743bf6d9698d9a5eddeeedc82
1RISHAV234 <83695942+1RISHAV234@users.noreply.github.com>
2021-05-05 19:17:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function windows7() {
+  alert("A dried plant is nothing but a sign to plant a new one - Priyansh Shah");
+}
+function windows8() {
+  alert("Plant a tree and get Oxygen for free - Unknown");
+}
+function windows10() {
+  alert("To plant a garden is to believe in tomorrow - Audrey Hepburn");
+}
+function windowsXP() {
+  alert("If you have a garden in your library, everything will be complete - Marcus Tullius Cicero");
+}
+function setDate() {
+    document.getElementById("text").innerHTML = new Date().toDateString();
+}
